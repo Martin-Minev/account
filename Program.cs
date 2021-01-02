dd583871d3da62f4dba11e952c1c69bcb90b1e4b
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-01-02 21:14:31 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace account
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int numOfOperations = int.Parse(Console.ReadLine());
+            int counter = 0;
+            double total = 0.00;
+            while (counter < numOfOperations)
+            {
+                double deposit = double.Parse(Console.ReadLine());
+                if (deposit < 0.00)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {deposit:F2}");
+                counter++;
+                total += deposit;
+            }
+
+            Console.WriteLine($"Total:{total:F2}");
+        }
+    }
+}
